a8b5e18c24372bd41dcdb58744f7696ec5275a5a
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-08-20 13:23:32 +0800

    init

---
+++ src/pages/stack.tsx
@@ -0,0 +1,44 @@
+import { trpc } from "../utils/trpc";
+
+type TechnologyCardProps = {
+  name: string;
+  description: string;
+  documentation: string;
+};
+
+const Stack = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+  return (
+    <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+        Create <span className="text-purple-300">T3</span> App
+      </h1>
+      <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
+        {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+      </div>
+    </main>
+  );
+};
+
+const TechnologyCard = ({
+  name,
+  description,
+  documentation,
+}: TechnologyCardProps) => {
+  return (
+    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
+      <h2 className="text-lg text-gray-700">{name}</h2>
+      <p className="text-sm text-gray-600">{description}</p>
+      <a
+        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
+        href={documentation}
+        target="_blank"
+        rel="noreferrer"
+      >
+        Documentation
+      </a>
+    </section>
+  );
+};
+
+export default Stack;
