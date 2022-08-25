5ba473abcb33ccfe1a47c18b72c2d7ab388e1e18
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-08-26 00:15:39 +0800

    update course feature

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,18 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { trpc } from "../utils/trpc";
+import { useQueryClient } from "react-query";
 
 const Dashboard = () => {
-  // const { isLoading, data } = trpc.useQuery(["user.getSession"]);
+  const queryClient = useQueryClient();
+  const { isLoading: isCourseLoading, data: userData } = trpc.useQuery([
+    "user.getUser",
+  ]);
+  const mutation = trpc.useMutation(["user.updateUserCourse"], {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["user.getUser"]);
+    },
+  });
   const router = useRouter();
   const { data: session, status } = useSession({
     required: true,
@@ -12,12 +21,38 @@ const Dashboard = () => {
     },
   });
   const user = session?.user;
+  const course = userData?.course;
+  let courseFormatted = "";
+  if (course) {
+    courseFormatted = `${course?.college} ${
+      course?.department + course?.courseNumber
+    } ${course?.section}`;
+  }
+
+  const updateCourse = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    mutation.mutate({
+      college: "CAS",
+      department: "CS",
+      courseNumber: "210",
+      section: "A1",
+    });
+  };
 
   return (
-    <>
+    <div className="dark:text-gray-200">
       <h1>Dashboard</h1>
-      <div>{status === "loading" ? "Loading..." : user?.name}</div>
-    </>
+      <div>
+        {status === "loading" ? "Loading..." : `Welcome, ${user?.name}!`}
+      </div>
+      <div>
+        {isCourseLoading ? "Loading..." : `Your course is ${courseFormatted}`}
+      </div>
+      {/* <input></input> */}
+      <button onClick={updateCourse}>
+        {mutation.isLoading ? "Updating..." : "Update"}
+      </button>
+    </div>
   );
 };
 
--- src/server/router/protected-example-router.ts
+++ src/server/router/protected-example-router.ts
@@ -1,10 +1,55 @@
+import { z } from "zod";
 import { createProtectedRouter } from "./protected-router";
 
 // Example router with queries that can only be hit if the user requesting is signed in
 export const protectedExampleRouter = createProtectedRouter()
-  .query("getSession", {
-    resolve({ ctx }) {
-      return ctx.session;
+  .query("getUser", {
+    async resolve({ ctx }) {
+      return await ctx.prisma.user.findUnique({
+        where: { id: ctx.session.user.id },
+        include: {
+          course: true,
+        },
+      });
+    },
+  })
+  .mutation("updateUserCourse", {
+    input: z.object({
+      college: z.string(),
+      department: z.string(),
+      courseNumber: z.string(),
+      section: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      let course = await ctx.prisma.course.findFirst({
+        where: {
+          college: input.college,
+          department: input.department,
+          courseNumber: input.courseNumber,
+          section: input.section,
+        },
+      });
+      if (!course) {
+        course = await ctx.prisma.course.create({
+          data: {
+            college: input.college,
+            department: input.department,
+            courseNumber: input.courseNumber,
+            section: input.section,
+          },
+        });
+      }
+      console.log("courseId: " + course.id)
+      return await ctx.prisma.user.update({
+        where: { id: ctx.session.user.id },
+        data: {
+          course: {
+            connect: {
+              id: course.id,
+            },
+          },
+        },
+      });
     },
   })
   .query("getSecretMessage", {
