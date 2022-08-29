d510a3a8b74a7abb1bd6e3c75248dd1343decf34
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-08-30 02:29:46 +0800

    fix eslint

--- src/components/MenuDropdown.tsx
+++ src/components/MenuDropdown.tsx
@@ -3,13 +3,14 @@ import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
+import { User } from "@prisma/client";
 
 // Menu Dropdown when user is logged in
-export default function MenuDropdown({ user }: any) {
+export default function MenuDropdown(user: User) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-        <Image src={user.image} alt="Profile photo" width={50} height={50} />
+        <Image src={user.image as string} alt="Profile photo" width={50} height={50} />
       </Menu.Button>
       <Transition
         as={Fragment}
