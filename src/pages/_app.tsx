8fb8ca50ed1dbd3f5d715a4af017064462a44caa
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-08-30 02:38:27 +0800

    update file name casing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,6 @@ import { SessionProvider } from "next-auth/react";
 import Header from "../components/Header";
 import "../styles/globals.css";
 
-
 const MyApp: AppType = ({
   Component,
   pageProps: { session, ...pageProps },
